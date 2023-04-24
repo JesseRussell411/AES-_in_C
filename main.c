c79aab7fcd02faaca3b22007fdc3e03331b11a83
Jesse Russell <jesserussell411@gmail.com>
2023-04-23 23:14:37 -0600

    it works

--- main.c
+++ main.c
@@ -58,6 +58,24 @@ uint8_t s_box[256] = {
     0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
     0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16};
 
+uint8_t s_box_inverse[256] = {
+    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB, 
+    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB, 
+    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E, 
+    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25, 
+    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92, 
+    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84, 
+    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06, 
+    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B, 
+    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73, 
+    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E, 
+    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B, 
+    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4, 
+    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F, 
+    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF, 
+    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61, 
+    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D};
+
 uint8_t mul2[256] = {
     0x00, 0x02, 0x04, 0x06, 0x08, 0x0a, 0x0c, 0x0e, 0x10, 0x12, 0x14, 0x16, 0x18, 0x1a, 0x1c, 0x1e,
     0x20, 0x22, 0x24, 0x26, 0x28, 0x2a, 0x2c, 0x2e, 0x30, 0x32, 0x34, 0x36, 0x38, 0x3a, 0x3c, 0x3e,
@@ -115,7 +133,7 @@ uint8_t rcon[256] = {
 
 
 // galois field multiplication
-inline uint8_t finiteFieldMultiply(uint8_t a, uint8_t b){
+static inline uint8_t finiteFieldMultiply(uint8_t a, uint8_t b){
     if (a == 0x00) return 0x00;
     if (b == 0x00) return 0x00;
     if (a == 0x01) return b;
@@ -133,17 +151,33 @@ static inline uint8_t mul(uint8_t a, uint8_t b){
     return finiteFieldMultiply(a, b);
 }
 
+
+
+// =====================
+//    key expansion
+// =====================
+static void rotWord(uint8_t *word){
+    // rotate 4 bytes to the left: 1234 -> 2341
+    uint32_t *q = (uint32_t *)word;
+    *q = (*q >> 8) | ((*q & 0xff) << 24);
+}
+
+static void subWord(uint8_t *word) {
+    for(int i = 0; i < 4; i++){
+        word[i] = s_box[word[i]];
+    }
+}
+
+
 void keyExpansionCore(uint8_t *in, uint8_t i)
 {
-    // rotate
-    uint32_t *q = (uint32_t *)in;
-    *q = (*q >> 8) | ((*q & 0xff) << 24);
+    // // rotate
+    // uint32_t *q = (uint32_t *)in;
+    // *q = (*q >> 8) | ((*q & 0xff) << 24);
+    rotWord(in);
 
     // s box
-    for (int i = 0; i < 4; i++)
-    {
-        in[i] = s_box[in[i]];
-    }
+    subWord(in);
 
     // Rcon
     in[0] ^= rcon[i];
@@ -176,13 +210,19 @@ void KeyExpansion(uint8_t *inputKey, uint8_t *expandedKeys)
     }
 }
 
-void SubBytes(uint8_t *state) {
+void subBytes(uint8_t *state) {
     for(int i = 0; i < 16; i++){
         state[i] = s_box[state[i]];
     }
 }
 
-void ShiftRows(uint8_t *state)
+void subBytes_inverse(uint8_t *state) {
+    for( int i = 0; i < 16; i++){
+        state[i] = s_box_inverse[state[i]];
+    }
+}
+
+void shiftRows(uint8_t *state)
 {
 
     // 00 04 08 12              \     00 04 08 12
@@ -190,10 +230,6 @@ void ShiftRows(uint8_t *state)
     // 02 06 10 14               /    10 14 02 06
     // 03 07 11 15              /     15 03 07 11
 
-    //
-    //
-    //
-    //
     uint8_t tmp[16];
 
     tmp[0] = state[0];
@@ -219,7 +255,7 @@ void ShiftRows(uint8_t *state)
     memcpy(state, tmp, 16);
 }
 
-void ShiftRowsReverse(uint8_t *state)
+void shiftRows_inverse(uint8_t *state)
 {
     // 00 04 08 12              \     00 04 08 12
     // 01 05 09 13    ___________\    13 01 05 09
@@ -251,7 +287,7 @@ void ShiftRowsReverse(uint8_t *state)
     memcpy(state, tmp, 16);
 }
 
-void MixColumns(uint8_t *state)
+void mixColumns(uint8_t *state)
 {
     //Galois field multiplication by this matrix of the matrix made up by the state's columns
     // |2 3 1 1| |00 05 09 13|
@@ -286,14 +322,14 @@ void MixColumns(uint8_t *state)
     memcpy(state, result, 16);
 }
 
-void mixColumnsReverse(uint8_t *state) {
+void mixColumns_inverse(uint8_t *state) {
     //Galois field multiplication by this matrix (inverse of the one used for the non-reverse mixColumns) of the matrix made up by the state's columns
     // |0E 0B 0D 09| |00 05 09 13|
     // |09 0E 0B 0D| |01 04 08 12|
     // |0D 09 0E 0B| |02 06 10 14|
     // |0B 0D 09 0E| |03 07 11 15|
-    //  special  state columns
-    //  matrix      matrix
+    //  special      state columns
+    //  matrix          matrix
 
     uint8_t result[16];
 
@@ -320,7 +356,7 @@ void mixColumnsReverse(uint8_t *state) {
     memcpy(state, result, 16);
 }
 
-void AddRoundKey(uint8_t *state, uint8_t *roundKey)
+void addRoundKey(uint8_t *state, uint8_t *roundKey)
 {
     for (int i = 0; i < 16; i++)
     {
@@ -333,28 +369,57 @@ void AES_encryption(uint8_t *message, uint8_t *key)
     uint8_t state[16];
     memcpy(state, message, 16);
 
-    int numberOfRounds = 9;
+    int numberOfRounds = 10;
 
     uint8_t expandedKey[176];
     KeyExpansion(key, expandedKey);
 
-    AddRoundKey(state, key);
+    addRoundKey(state, key);
 
-    for (int i = 1; i <= numberOfRounds; i++)
+    for (int i = 1; i <= numberOfRounds - 1; i++)
     {
-        SubBytes(state);
-        ShiftRows(state);
-        MixColumns(state);
-        AddRoundKey(state, expandedKey + 16 * i);
+        subBytes(state);
+        shiftRows(state);
+        mixColumns(state);
+        addRoundKey(state, expandedKey + 16 * i);
     }
 
     // final round
-    SubBytes(state);
-    ShiftRows(state);
-    AddRoundKey(state, expandedKey + 16 * (numberOfRounds + 1));
+    subBytes(state);
+    shiftRows(state);
+    addRoundKey(state, expandedKey + 16 * (numberOfRounds));
 
     memcpy(message, state, 16);  
 }
+
+void aes_decryption(uint8_t *message, uint8_t *key)
+{
+    uint8_t state[16];
+    memcpy(state, message, 16);
+
+    int numberOfRounds = 10;
+
+    uint8_t expandedKey[176];
+    KeyExpansion(key, expandedKey);
+
+    // final round
+    addRoundKey(state, expandedKey + 16 * (numberOfRounds));
+    shiftRows_inverse(state);
+    subBytes_inverse(state);
+
+    for(int i = numberOfRounds - 1; i >= 1; i--){
+        addRoundKey(state, expandedKey + 16 * i);
+        mixColumns_inverse(state);
+        shiftRows_inverse(state);
+        subBytes_inverse(state);
+    }
+
+    addRoundKey(state, key);
+
+    memcpy(message, state, 16);  
+}
+
+
 
 int main()
 {
@@ -386,26 +451,32 @@ int main()
     }
     printf("\n");
 
+    printf(" encrypt...");
+    printf(" %s\n", message);
+    printf(" decrypt...");
+    aes_decryption(message, key);
+    printf(" %s\n", message);
 
-    for(int i = 0; i < 16; i++){
-        printf(" %X", key[i]);
-    }
-    printf("\n");
+
+    // for(int i = 0; i < 16; i++){
+    //     printf(" %X", key[i]);
+    // }
+    // printf("\n");
 
 
-    ShiftRows(key);
+    // shiftRows(key);
 
-    for(int i = 0; i < 16; i++){
-        printf(" %X", key[i]);
-    }
-    printf("\n");
+    // for(int i = 0; i < 16; i++){
+    //     printf(" %X", key[i]);
+    // }
+    // printf("\n");
 
-    ShiftRowsReverse(key);
+    // shiftRows_inverse(key);
 
-    for(int i = 0; i < 16; i++){
-        printf(" %X", key[i]);
-    }
-    printf("\n");
+    // for(int i = 0; i < 16; i++){
+    //     printf(" %X", key[i]);
+    // }
+    // printf("\n");
 
     return 0;
 }
